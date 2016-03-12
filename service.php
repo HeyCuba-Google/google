bf43b8b6b00f4065a41a2dba207f4dedb4db3fac
hax0ran <magarr1234@gmail.com>
2016-03-12 13:47:57 -0500

    Update service.php

--- service.php
+++ service.php
@@ -2,7 +2,7 @@
 
  use Goutte\Client;
 
-class [Google] extends Service
+class Google extends Service
 {
 	/*
 	 * Function executed when the service is called
@@ -12,7 +12,7 @@ class [Google] extends Service
 	 * */
 	public function _main(Request $request)
 	{
-
+    /*
 		// create a new client
 		$client = new Client();
 		$guzzle = $client->getClient();
@@ -26,11 +26,23 @@ class [Google] extends Service
 		$result = $crawler->filter('.health-topic-boost-wrapper')->text();
 		*/
 
+    $web = file_get_contents("http://google.com");
+    echo $web;
+
+    exit;
+
+    $responses = array(
+      array("title"=>"Page Title One", "url"=>"http://url.one", "note"=>"description of the page"),
+      array("title"=>"Page Title Ten", "url"=>"http://url.one", "note"=>"description of the page"),
+      array("title"=>"Page Title two", "url"=>"http://url.one", "note"=>"description of the page"),
+      array("title"=>"Page Title Three", "url"=>"http://url.one", "note"=>"description of the page"),
+      array("title"=>"Page Title Four", "url"=>"http://url.one", "note"=>"description of the page")
+    );
+
 		// create a json object to send to the template
 		$responseContent = array(
-			"var_one" => "hello",
-			"var_two" => "world",
-			"var_three" => 23
+			"query" => "racing cars",
+			"responses" => $responses
 		);
 
 		// create the response
